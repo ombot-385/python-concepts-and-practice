b570be741c16e4b6fcc4f16ada1e1bb8a29bc288
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-20 20:06:39 +0530

    Update and rename fstring.py to 04-fstring.py

---
+++ DAY02-Data_types/04-fstring.py
@@ -0,0 +1,5 @@
+score=0
+height=1.8
+is_winning=True
+
+print(f"you score is {score},you height is {height},you are winning is {is_winning}")
--- DAY02-Data_types/fstring.py
+++
@@ -1,5 +0,0 @@
-score=0
-height=1.8
-is_winning=True
-
-print(f"you score is {score},you height is {height},you are winning is {is_winning}")
