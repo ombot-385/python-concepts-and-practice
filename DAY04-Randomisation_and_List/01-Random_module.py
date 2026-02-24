0d702239692c53994d478ea6ee7252b69a386dad
ombot-385 <omkar.kumar.dev@gmail.com>
2026-02-24 15:21:26 +0530

    Add files via upload

---
+++ DAY04-Randomisation_and_List/01-Random_module.py
@@ -0,0 +1,9 @@
+import random
+a=random.randint(1,10)
+print(a)
+
+b=random.random()
+print(b)
+
+c=random.uniform(1,10)
+print(c)
---
+++ DAY04-Randomisation_and_List/05-list2.py
@@ -0,0 +1,10 @@
+fruits=["strawberry","nectarines","apples","grapes","cherries"]
+vegetables=["spinach","kale","tomatoes","ladyfinger","potatoes"]
+dirty_dozen=[fruits,vegetables]
+print(dirty_dozen)
+
+fruits[-1]="melons"
+fruits.append("lemons")
+print(dirty_dozen)
+
+print(dirty_dozen[1][1])
