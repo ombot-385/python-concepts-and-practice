789513d5f19107da57b8321ab927332b7d667c3b
ombot-385 <omkar.kumar.dev@gmail.com>
2026-04-08 19:15:28 +0530

    Delete DAY03-Conditional_statement directory

--- DAY03-Conditional_statement/01-if_else.py
+++
@@ -1,9 +0,0 @@
-print("welcome to rollercoaster ride")
-height=int(input("enter your height in cm"))
-
-if(height>120):{
-    print("you can take a ride")
-}
-else:{
-    print("sorry you have to grow taller before you can ride")
-}
